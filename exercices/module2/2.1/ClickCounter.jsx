4ef877d3b956941bdeb054e467f0056b5c155926
LukenV <alexis.arnaud@student.vinci.be>
2024-09-17 22:16:19 +0200

    new:ex2.1-events

--- exercices/module2/2.1/ClickCounter.jsx
+++ exercices/module2/2.1/ClickCounter.jsx
@@ -2,12 +2,35 @@ import {useState} from "react";
 
 const ClickCounter = ({title, message}) => {
     const [count, setCount] = useState(0)
+    const [mouseEntered, setMouseEntered] = useState(false);
+
+    const clickMeMessage = "Please click on me now !";
+
+    const handleMouseEnter = () => {
+        setMouseEntered(true);
+    }
+
+    const handleMouseLeave = () => {
+        setMouseEntered(false);
+    }
+
+    const handleClick = () => {
+        setCount((count) => count + 1)
+    }
+
     return (
         <>
             <h1>{title}</h1>
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
+            <div className="container">
+                {!mouseEntered && (
+                    <div className="clickMeMessage">
+                        {clickMeMessage}
+                    </div>
+                )}
+                <button id="clickCounter" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
+                    count is {count}
+                </button>
+            </div>
             <p>{count >= 10 ? message : ""}</p>
         </>
     )
