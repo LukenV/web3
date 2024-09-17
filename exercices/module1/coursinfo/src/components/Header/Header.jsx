588791e40725a0614e750b0ae4b487ac45f5058c
LukenV <alexis.arnaud@student.vinci.be>
2024-09-17 16:59:29 +0200

    1.1-1.2 : import image

--- exercices/module1/coursinfo/src/components/Header/Header.jsx
+++ exercices/module1/coursinfo/src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
+import logoVinciPath from './HE_VINCI.png';
+
 const Header = (props) => {
     return (
-        <h1>
-            {props.title}
-        </h1>
+        <>
+            <h1>
+                {props.title}
+            </h1>
+            <div className="imageContainer">
+                <img src={logoVinciPath} alt="Logo Vinci"/>
+            </div>
+        </>
     );
 }
 
