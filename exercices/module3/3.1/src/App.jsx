65046545e4c53ecad025fb73303b31a79ffd6146
LukenV <alexis.arnaud@student.vinci.be>
2024-09-29 23:57:19 +0200

    module3 : basics

---
+++ exercices/module3/3.1/src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import './App.css'
+
+const Button = ({handler, text}) => {
+    return (
+        <button onClick={handler}>
+            {text}
+        </button>
+    );
+}
+
+const Statistics = ({good, neutral, bad, total, average, positivePercentage}) => {
+    if (total === 0) {
+        return <p>No feedback given</p>
+    }
+    return (
+        <>
+            <table>
+                <tbody>
+                    <StatisticLine text="Good" value={good}/>
+                    <StatisticLine text="Neutral" value={neutral}/>
+                    <StatisticLine text="Bad" value={bad}/>
+                    <StatisticLine text="All" value={total}/>
+                    <StatisticLine text="Average" value={average / total}/>
+                    <StatisticLine text="Positive" value={(positivePercentage / total)*100}/>
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+const StatisticLine = ({value, text}) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    );
+}
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [average, setAverage] = useState(0);
+    const [positivePercentage, setPositivePercentage] = useState(0);
+
+    const handlerGood = () => {
+        const updatedGood = good + 1;
+        setGood(updatedGood);
+        const updatedTotal = total + 1;
+        setTotal(updatedTotal);
+        setAverage(average + 1);
+        setPositivePercentage(positivePercentage+1);
+    }
+    const handlerNeutral = () => {
+        const updateNeutral = neutral + 1;
+        setNeutral(updateNeutral);
+        const updatedTotal = total + 1;
+        setTotal(updatedTotal);
+    }
+    const handlerBad = () => {
+        const updateBad = bad + 1;
+        setBad(updateBad);
+        const updatedTotal = total + 1;
+        setTotal(updatedTotal);
+        setAverage(average - 1);
+    }
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+                <Button handler={handlerGood} text="Good"/>
+                <Button handler={handlerNeutral} text="Neutral"/>
+                <Button handler={handlerBad} text="Bad"/>
+            <h1>Statistics</h1>
+
+            <Statistics total={total} good={good}
+                        neutral={neutral} bad={bad} average={average}
+                        positivePercentage={positivePercentage}/>
+        </div>
+    );
+}
+
+export default App
---
+++ exercices/module3/clicksLeftRight/src/App.jsx
@@ -0,0 +1,58 @@
+import './App.css'
+import {useState} from "react";
+
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                The app is used by pressing the buttons
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            Button press history : {props.allClicks.join(' ')}
+        </div>
+    )
+}
+
+const Button = ({handleClick, text}) => {
+    return (
+        <button onClick={handleClick}>
+            {text}
+        </button>
+    )
+}
+
+function App() {
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+    const [allClicks, setAllClicks] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    const handleLeftClick = () => {
+        setAllClicks(allClicks.concat('L'));
+        const updatedLeft = left+1;
+        setLeft(updatedLeft);
+        setTotal(updatedLeft+right);
+    }
+    const handleRightClick = () => {
+        setAllClicks(allClicks.concat('R'));
+        const updatedRight = right+1;
+        setRight(updatedRight);
+        setTotal(left + updatedRight);
+    }
+    return (
+        <div>
+            {left}
+            <Button onClick={handleLeftClick} text="Left"/>
+            <Button onClick={handleRightClick} text="Right"/>
+            {right}
+            <History allClicks={allClicks}/>
+            <p>Total : {total}</p>
+        </div>
+    )
+}
+
+export default App
